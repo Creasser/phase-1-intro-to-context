f26de9c10a2f558d22ae072bad97a154d69e9460
Blake Creasser <blakecreasser@aggienetwork.com>
2022-11-28 22:24:14 -0600

    commit tree f05fde9ad87dcb29812f012781a0cb1945d44b01

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+function createEmployeeRecord(arr){
+    let employeeObj = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [] ,
+        timeOutEvents: [] ,
+    }
+    return employeeObj
+}
+
+
+function createEmployeeRecords(arr){
+    let employeeArr = []
+    arr.forEach(e => {
+        employeeArr.push(createEmployeeRecord(e))
+    });
+    return employeeArr
+}
+
+function createTimeInEvent(obj, string){
+    let dateArr = string.split(' ')
+    let hour = dateArr.pop()
+    let date = dateArr.shift()
+    obj.timeInEvents = [{
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date: date,
+    }]
+    // console.log(obj)
+    return obj
+}
+
+function createTimeOutEvent(obj, string){
+    let dateArr = string.split(' ')
+    let hour = dateArr.pop()
+    let date = dateArr.shift()
+    obj.timeOutEvents = [{
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date: date,
+    }]
+    // console.log(obj)
+    return obj
+}
+
+function hoursWorkedOnDate(obj, date){
+    if (date === obj.timeInEvents[0].date){
+        let hours = obj.timeInEvents[0].hour - obj.timeOutEvents[0].hour
+        
+        let hoursWorked = Math.abs(hours) / 100
+        return hoursWorked
+    }
+}
+
+function wagesEarnedOnDate(obj, date){
+    let hoursWorked = hoursWorkedOnDate(obj, date)
+    let wageOwed = hoursWorked * obj.payPerHour
+    return wageOwed
+}
